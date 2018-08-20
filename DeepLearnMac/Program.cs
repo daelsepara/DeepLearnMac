26a170569de70c2cdaafc6b80a9f47f8732ad8e4
Dael <dael.separa@gmail.com>
2018-08-20 14:06:07 +0200

    revert to old-style out statement for VS2015 compatibility

--- DeepLearnMac/Program.cs
+++ DeepLearnMac/Program.cs
@@ -3,45 +3,45 @@ using Gtk;
 
 namespace DeepLearnMac
 {
-    static class MainClass
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        public static void Main()
-        {
-            try
-            {
+	static class MainClass
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		public static void Main()
+		{
+			try
+			{
 				bool owned;
 
-                var hMutex = new System.Threading.Mutex(true, "DeepLearnMac (BETA)", out owned);
+				var hMutex = new System.Threading.Mutex(true, "DeepLearnMac (BETA)", out owned);
 
-                if (owned)
-                {
-                    Application.Init();
+				if (owned)
+				{
+					Application.Init();
 
-                    MainWindow win = new MainWindow();
-                    win.Show();
+					MainWindow win = new MainWindow();
+					win.Show();
 
-                    Application.Run();
-                }
-                else
-                {
-                    Console.WriteLine("Application is already running");
+					Application.Run();
+				}
+				else
+				{
+					Console.WriteLine("Application is already running");
 
-                    return;
-                }
+					return;
+				}
 
-                GC.KeepAlive(hMutex);
+				GC.KeepAlive(hMutex);
 
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error: {0}", ex.Message);
-            }
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Error: {0}", ex.Message);
+			}
 
-            return;
-        }
-    }
+			return;
+		}
+	}
 }
